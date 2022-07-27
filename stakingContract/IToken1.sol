eb200211720ac5d84df73ecc5e93547fda2b5e8c
shadow <sha009dow@gmail.com>
2022-07-27 10:25:52 +0500

    Revert "complete project"
    This reverts commit 0716c581b31795a58ef46d7540a196a842aca3eb.

--- stakingContract/IToken1.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface Token1{
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
-    function transfer(address recipient, uint256 amount) external returns (bool); 
-}
